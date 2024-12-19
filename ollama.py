7c82a6f96b463e4e45b6f70ab658f625145118d0
MaeSosto <mae.sosto@gmail.com>
2024-12-19 15:10:12 +0100

    Gemma2 sentences generated!

--- ollama.py
+++ ollama.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Initialisation
-
-# In[1]:
-
-
-#get_ipython().system('pip3 install torch')
-
-
-# # Imports
-
-# In[2]:
-
-
 import torch
 import logging
 import requests
@@ -28,23 +12,14 @@ print(f"PyTorch version: {torch.__version__}")
 device = "mps" if torch.backends.mps.is_available() else torch.device("cuda") if torch.cuda.is_available() else torch.device('cpu')
 print(f"Using device: {device}")
 
-
-# # Global Variables
-
-# In[3]:
-
-
 #Data Source
 OUTPUT_TEMPLATE = 'output_template/'
 TEMPLATES_COMPLETE_PATH = OUTPUT_TEMPLATE + 'template_complete.csv'
 DATA_SOURCE = 'dataset_source/'
 OUTPUT_PREDICTION = 'output_prediction/'
-
-
-# In[4]:
-
-
 url = "http://localhost:11434/api/generate"
+LLAMA3_MODEL = 'llama3'
+GEMMA2_MODEL = 'gemma2'
 
 class generateSentences():
     def __init__(self, modelName):
@@ -55,11 +30,8 @@ class generateSentences():
                 "Generated": []
             }
             print("Generating sentences...")
-            for index,row in tqdm(self.templateFile.iterrows(), total=self.templateFile.shape[0], desc='Generating ', unit=' sentences'):
+            for index,row in tqdm(self.templateFile.iterrows(), total=self.templateFile.shape[0], desc=f'Generating with {self.modelName} model', unit=' sentences'):
                 sentence = row.loc['template']
-                #Other propts to try:
-                #prompt = f"Complete the follwing sentence and provide the response {sentence}"
-                #prompt = "Complete the follwing sentence (return only one response): {0}".format(sentence)
                 
                 prompt = f"Complete the following sentence: `{sentence}` Provide only the additional words necessary to complete the sentence as output, without repeating the initial part or adding any explanations."
                 response = self.modelRequest(prompt)
@@ -67,7 +39,6 @@ class generateSentences():
                 dicSentences["Generated"].append(sentence + response)
                 print(str(index) +"-"+ sentence + response)
             df = pd.DataFrame.from_dict(dicSentences)    
-            #display(df)
             print("Sentences generated!")            
             os.makedirs(OUTPUT_PREDICTION, exist_ok=True)
             df.to_csv(OUTPUT_PREDICTION+self.modelName+'.csv', sep=';', index=False)
@@ -91,16 +62,8 @@ class generateSentences():
         }
 
         response = requests.post(url, headers=headers, json=data)
-        #return (response.json()['choices'][0]['message']['content'])
         response = response.json()['response'].replace("`", "").replace('\"', '')
         return(response)
     
-
-
-# In[5]:
-
-
-Llama3 = 'llama3'
-gemma2 = 'gemma2'
 generateSentences(Llama3)
 
