6998791da5da4cf5005700e31b448947b9fa3eae
MaeSosto <mae.sosto@gmail.com>
2024-12-22 19:12:00 +0100

    Open AI and Google models added

---
+++ generateSentences.py
@@ -0,0 +1,87 @@
+from utils import * 
+
+
+class generateSentences():
+    def __init__(self, modelName):
+            self.modelName = modelName
+            self.templateFile = pd.read_csv(TEMPLATES_COMPLETE_PATH, sep=";")
+            if self.modelName == GEMINI_FLASH:
+                self.genai =genai.configure(api_key=GENAI_API_KEY)
+            if self.modelName == GPT4 or self.modelName == GPT4_MINI:
+                self.client = OpenAI(api_key=OPENAI_API_KEY)
+            dicSentences = {
+                "Template": [],
+                "Generated": []
+            }
+            print("Generating sentences...")
+            for index,row in tqdm(self.templateFile[:20].iterrows(), total=self.templateFile.shape[0], desc=f'Generating with {self.modelName} model', unit=' sentences'):
+                sentence = row.loc['template']
+                
+                prompt = f"Complete the following sentence: `{sentence}` Provide only the additional words necessary to complete the sentence as output, without repeating the initial part or adding any explanations."
+                if self.modelName == GEMINI_FLASH:
+                    time.sleep(2.5)
+                    response = self.geminiRequest(prompt)
+                elif self.modelName == GPT4 or self.modelName == GPT4_MINI:
+                    response = self.GPTRequest(prompt)
+                else:
+                    response = self.ollamaRequest(prompt)
+                dicSentences["Template"].append(sentence)
+                dicSentences["Generated"].append(sentence + response)
+                print(str(index) +"-"+ sentence + response)
+            df = pd.DataFrame.from_dict(dicSentences)    
+            print("Sentences generated!")            
+            os.makedirs(OUTPUT_PREDICTION, exist_ok=True)
+            df.to_csv(OUTPUT_PREDICTION+self.modelName+'.csv', sep=';', index=False)
+            print("File generated!!")
+    
+    def ollamaRequest (self, prompt):
+        data = {
+            "model": self.modelName,
+            "prompt": prompt,
+            "messages": [
+                {
+                "role": "user",
+                "content": prompt
+                }
+            ],   
+            "stream": False
+        }
+        
+        headers = {
+            "Content-Type": 'application/json'
+        }
+
+        response = requests.post(URL_OLLAMA_LOCAL, headers=headers, json=data)
+        response = response.json()['response'].replace("`", "").replace('\"', '')
+        return(response)
+
+    def geminiRequest(self, prompt):
+        model = self.genai.GenerativeModel(GEMINI_FLASH)
+        response = model.generate_content(prompt).text
+        return response
+    
+    def GPTRequest(self, prompt):
+        completion = self.client.chat.completions.create(
+            model= self.modelName,
+            store=True,
+            messages=[
+                {
+                    "role": "user", 
+                    "content": prompt
+                }
+            ]
+            )
+        print(f"GPT resp: {completion}")
+        response = completion.choices[0].message.content
+        print(f"GPT resp: {response}")
+        return response
+
+chosenModel = -1
+while chosenModel < 0 or chosenModel > len(MODEL_LIST)-1:
+    print('Select a model: ')
+    for idx, x in enumerate(MODEL_LIST):
+        print(f"[{idx}] -  {x}")
+    chosenModel = int(input())
+
+print(MODEL_LIST[chosenModel])
+generateSentences(MODEL_LIST[chosenModel])
--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import requests
 from tqdm import tqdm
 import pandas as pd
 import os
+import time
+import google.generativeai as genai
+from openai import OpenAI
 
 logging.basicConfig(level=logging.INFO)# OPTIONAL
 print(f"PyTorch version: {torch.__version__}")
@@ -37,9 +40,16 @@ DATA_SOURCE = 'dataset_source/'
 OUTPUT_PREDICTION = 'output_prediction/'
 
 #Ollama Models
-url = "http://localhost:11434/api/generate"
+URL_OLLAMA_LOCAL = "http://localhost:11434/api/generate"
 LLAMA3 = 'llama3'
 LLAMA3_3 = 'llama3.3'
 GEMMA2 = 'gemma2'
 GEMMA2_27B = 'gemma2:27b'
-MODEL_LIST = [LLAMA3, LLAMA3_3, GEMMA2, GEMMA2_27B]
+GEMINI_FLASH = "gemini-1.5-flash"
+GPT4_MINI = 'gpt-4o-mini'
+GPT4 = 'gpt-4o'
+MODEL_LIST = [LLAMA3, LLAMA3_3, GEMMA2, GEMMA2_27B, GEMINI_FLASH, GPT4, GPT4_MINI]
+
+#API KEY
+GENAI_API_KEY = 'AIzaSyAk29hD3nisZTXfr14rK7rdn_pZ_dQ8jDI'
+OPENAI_API_KEY = "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"
