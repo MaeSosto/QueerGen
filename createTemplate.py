b091fd8d92a8266bbd3b404e892c7f3673ac8c6f
Mae Sosto <44461808+MaeSosto@users.noreply.github.com>
2025-03-06 12:22:59 +0100

    Dataset update and tempalte generation - Done

--- createTemplate.py
+++ createTemplate.py
@@ -9,17 +9,37 @@ def createTemplate():
     print("๏ Generating template...")
     
     for _,row in tqdm(templateFile.iterrows(), total=templateFile.shape[0], desc='Creating template', unit=' s'):
-        #Creating sentences with nouns and pronouns
-        for _, r in pd.read_csv(NOUNS_PATH).iterrows():
-            sentence = row.loc[TEMPLATE].replace(TARGET_, f"The {r.loc[SUBJECT]}" + f'{" person" if r.loc[PERSON] == "y" else ""}')
-            
+        nounsFile = pd.read_csv(NOUNS_PATH)
+        neutralList = nounsFile[nounsFile[TYPE] == NEUTRAL].copy()
+        #neutralList = neutralList[SUBJECT].tolist()
+        nonNeutralList = nounsFile[nounsFile[TYPE] != NEUTRAL].copy()
+        
+        template = row.loc[TEMPLATE]
+        #Creating sentences with nouns
+        for _, neu in neutralList.iterrows():
             dataList.append([
-                row.loc[TEMPLATE],
-                r.loc[SUBJECT], #subject
-                sentence, #new template
-                r.loc[TYPE], #type
-                r.loc[CATEGORY] #category
-            ]) 
+                template, #original
+                neu.loc[SUBJECT], #subject
+                template.replace(TARGET_, f"The {neu.loc[SUBJECT]}"), #template
+                neu.loc[TYPE], #type
+                neu.loc[CATEGORY] #category
+            ])    
+            for _,r in nonNeutralList.iterrows():
+                if r.loc[PERSON] == "y":
+                    subject = f"The {r.loc[SUBJECT]} {neu.loc[SUBJECT]}"
+                elif r.loc[PERSON] == "n" and neu.loc[SUBJECT] == 'person':
+                    subject = f"The {r.loc[SUBJECT]}"
+                else:
+                    subject = f"The {neu.loc[SUBJECT]} is a {r.loc[SUBJECT]} and"
+                sentence = template.replace(TARGET_, subject)
+                
+                dataList.append([
+                    template, #original
+                    r.loc[SUBJECT], #subject
+                    sentence, #template
+                    r.loc[TYPE], #type
+                    r.loc[CATEGORY] #category
+                ]) 
     data_df = pd.DataFrame(dataList, columns=[ ORIGINAL, SUBJECT, TEMPLATE, TYPE, CATEGORY])
     data_df.to_csv(TEMPLATE_PATH_COMPLETE, index_label = 'index')
     print("๏ File template generated!")
